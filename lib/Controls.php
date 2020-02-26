e2b2b4fe710179fc12818d03484cc348a9bc7d6f
Sovit Tamrakar <sovit.tamrakar@gmail.com>
2020-02-26 18:26:13 +0545

    update

---
+++ lib/Controls.php
@@ -0,0 +1,167 @@
+<?php
+namespace Sovit;
+
+use Sovit\Helper;
+
+if (!\defined('ABSPATH')) {
+    exit; // Exit if accessed directly.
+}
+if (!class_exists("\Sovit\Controls")) {
+
+    class Controls {
+        /**
+         * @param array $field
+         * @return null
+         */
+        public static function render($field = []) {
+            if (empty($field) || empty($field['id'])) {
+                return;
+            }
+
+            $defaults = [
+                'type'       => '',
+                'attributes' => [],
+                'std'        => '',
+                'desc'       => '',
+            ];
+
+            $field = array_merge($defaults, $field);
+
+            $method_name = $field['type'];
+
+            if (!method_exists(__CLASS__, $method_name)) {
+                $method_name = 'text';
+            }
+            self::$method_name($field);
+        }
+
+        /**
+         * @param array $field
+         */
+        private static function checkbox(array $field) {
+            echo '<label class="switch">';
+            echo '<input type="checkbox" id="' . esc_attr($field['id']) . '" name="' . esc_attr($field['name']) . '" value="yes" ';
+            checked($field['value'], 'yes');
+            echo ' />';
+            echo '<span></span>';
+            echo '</label>';
+            if (!empty($field['sub_desc'])) {
+                echo $field['sub_desc'];
+            }
+            if (!empty($field['desc'])) {
+                echo '<p class="description">';
+                echo $field['desc'];
+                echo '</p>';
+            }
+        }
+
+        /**
+         * @param array $field
+         */
+        private static function checkbox_list(array $field) {
+            if (!\is_array($field['value'])) {
+                $field['value'] = [];
+            }
+            foreach ($field['options'] as $option) {
+                echo '<p><label class="switch">';
+                echo '<input type="checkbox" name="' . esc_attr($field['name']) . '[]" value="' . esc_attr($option['value']) . '" ';
+                checked(\in_array($option['value'], $field['value']));
+                echo '/>';
+                echo '<span></span>';
+
+                echo '</label> ';
+                echo $option['name'];
+                echo '</p>';
+            }
+            if (!empty($field['sub_desc'])) {
+                echo $field['sub_desc'];
+            }
+            if (!empty($field['desc'])) {
+                echo '<p class="description">';
+                echo $field['desc'];
+                echo '</p>';
+            }
+        }
+
+        /**
+         * @param array $field
+         */
+        private static function colorpicker(array $field) {
+            $attributes = Helper::render_html_attributes($field['attributes']);
+            echo '<input type="hidden" id="' . esc_attr($field['id']) . '" name="' . esc_attr($field['name']) . '" value="' . esc_attr($field['value']) . '"/>';
+            echo '<span class="colorpicker-pickr" data-value="' . esc_attr($field['value']) . '" data-target="' . esc_attr($field['id']) . '" data-default="' . $field['std'] . '"></span>';
+            if (!empty($field['sub_desc'])) {
+                echo $field['sub_desc'];
+            }
+            if (!empty($field['desc'])) {
+                echo '<p class="description">';
+                echo $field['desc'];
+                echo '</p>';
+            }
+        }
+
+        /**
+         * @param array $field
+         * @return null
+         */
+        private static function raw_html(array $field) {
+            if (empty($field['html'])) {
+                return;
+            }
+            echo '<div id="' . $field['id'] . '">';
+
+            echo '<div>' . $field['html'] . '</div>';
+
+            echo '</div>';
+        }
+
+        /**
+         * @param array $field
+         */
+        private static function select(array $field) {
+            $attributes = Helper::render_html_attributes($field['attributes']);
+
+            echo '<select name="' . esc_attr($field['name']) . '" id="' . $field['id'] . '" ' . $attributes . '>';
+            if (!empty($field['show_select'])) {
+                echo '<option value="">— ' . esc_html__('Select') . ' —</option>';
+            }
+
+            foreach ($field['options'] as $value => $label) {
+                echo '<option value="' . esc_attr($value) . '" ';
+                selected($value, $field['value']);
+                echo '>' . $label . '</option>';
+            }
+            echo '</select>';
+
+            if (!empty($field['sub_desc'])) {
+                echo $field['sub_desc'];
+            }
+            if (!empty($field['desc'])) {
+                echo '<p class="description">';
+                echo $field['desc'];
+                echo '</p>';
+            }
+        }
+
+        /**
+         * @param array $field
+         */
+        private static function text(array $field) {
+            if (empty($field['attributes']['class'])) {
+                $field['attributes']['class'] = 'regular-text';
+            }
+
+            $attributes = Helper::render_html_attributes($field['attributes']);
+            echo '<input type="' . esc_attr($field['type']) . '" id="' . esc_attr($field['id']) . '" name="' . esc_attr($field['name']) . '" value="' . esc_attr($field['value']) . '" ' . $attributes . '/>';
+
+            if (!empty($field['sub_desc'])) {
+                echo $field['sub_desc'];
+            }
+            if (!empty($field['desc'])) {
+                echo '<p class="description">';
+                echo $field['desc'];
+                echo '</p>';
+            }
+        }
+    }
+}
---
+++ lib/Settings.php
@@ -0,0 +1,468 @@
+<?php
+namespace Sovit;
+if (!class_exists("\Sovit\Settings")) {
+    class Settings {
+        /**
+         * @var mixed
+         */
+        public $page = null;
+
+        /**
+         * @var string
+         */
+        private $capability = "manage_options";
+
+        /**
+         * @var string
+         */
+        private $menu_icon = "";
+
+        /**
+         * @var mixed
+         */
+        private $menu_position = null;
+
+        /**
+         * @var mixed
+         */
+        private $menu_title;
+
+        /**
+         * @var mixed
+         */
+        private $mernu_parent = false;
+
+        /**
+         * @var mixed
+         */
+        private $page_id = "sovit-settings";
+
+        /**
+         * @var mixed
+         */
+        private $page_title = "Settings";
+
+        /**
+         * @var string
+         */
+        private $setting_key = "sovit_setting";
+
+        /**
+         * @var mixed
+         */
+        private $tabs = null;
+
+        /**
+         * @param $page_id
+         * @return mixed
+         */
+        public function __construct($page_id = false, $setting_key = false, $page_title = false, $menu_title = false) {
+            add_action('admin_menu', [$this, 'admin_menu'], 20);
+            if (!empty($_POST['option_page']) && $page_id === $_POST['option_page']) {
+                add_action('admin_init', [$this, 'register_settings_fields']);
+            }
+            add_filter('wppress/settings/field_args', [$this, 'field_args'], 4, 10);
+            if ($page_id !== false) {
+                $this->set_page_id($page_id);
+            }
+            if ($page_title !== false) {
+                $this->set_page_title($page_title);
+            }
+            if ($menu_title !== false) {
+                $this->set_menu_title($menu_title);
+            }if ($setting_key !== false) {
+                $this->set_setting_key($setting_key);
+            }
+            add_action('admin_enqueue_scripts', [$this, 'admin_enqueue_scripts']);
+
+            return $this;
+        }
+
+        /**
+         * @param $tab_id
+         * @param $section_id
+         * @param $field_id
+         * @param array $field_args
+         * @return mixed
+         */
+        public function add_field($tab_id, $section_id, $field_id, array $field_args) {
+            $this->ensure_tabs();
+
+            if (!isset($this->tabs[$tab_id])) {
+                // If the requested tab doesn't exists, use the first tab
+                $tab_id = key($this->tabs);
+            }
+
+            if (!isset($this->tabs[$tab_id]['sections'][$section_id])) {
+                // If the requested section doesn't exists, use the first section
+                $section_id = key($this->tabs[$tab_id]['sections']);
+            }
+
+            if (isset($this->tabs[$tab_id]['sections'][$section_id]['fields'][$field_id])) {
+                // Don't override an existing field
+                return;
+            }
+
+            $this->tabs[$tab_id]['sections'][$section_id]['fields'][$field_id] = $field_args;
+            return $this;
+        }
+
+        /**
+         * @param $tab_id
+         * @param $section_id
+         * @param array $fields
+         */
+        final public function add_fields($tab_id, $section_id, array $fields) {
+            foreach ($fields as $field_id => $field_args) {
+                $this->add_field($tab_id, $section_id, $field_id, $field_args);
+            }
+            return $this;
+        }
+
+        /**
+         * @param $tab_id
+         * @param $section_id
+         * @param array $section_args
+         * @return null
+         */
+        final public function add_section($tab_id, $section_id, array $section_args = []) {
+            $this->ensure_tabs();
+
+            if (!isset($this->tabs[$tab_id])) {
+                // If the requested tab doesn't exists, use the first tab
+                $tab_id = key($this->tabs);
+            }
+
+            if (isset($this->tabs[$tab_id]['sections'][$section_id])) {
+                // Don't override an existing section
+                return;
+            }
+
+            if (!isset($section_args['fields'])) {
+                $section_args['fields'] = [];
+            }
+
+            $this->tabs[$tab_id]['sections'][$section_id] = $section_args;
+            return $this;
+        }
+
+        /**
+         * @param $hook
+         */
+        public function admin_enqueue_scripts($hook) {
+
+            if ($hook == $this->page) {
+                wp_register_script('pickr', \Sovit\Helper::get_file_url(dirname(__FILE__) . '/assets/pickr.min.js'), ['jquery'], null, true);
+                wp_enqueue_script($this->page_id . '-setting-js', \Sovit\Helper::get_file_url(dirname(__FILE__) . '/assets/settings.min.js'), ['jquery', 'pickr'], null, true);
+                wp_register_style('pickr-css', \Sovit\Helper::get_file_url(dirname(__FILE__) . "/assets/pickr.css"), []);
+                wp_enqueue_style($this->page_id . '-setting-css', \Sovit\Helper::get_file_url(dirname(__FILE__) . "/assets/settings.min.css"), ['pickr-css']);
+
+            }
+        }
+
+        public function admin_menu() {
+            if ($this->menu_parent === false) {
+                $this->page = add_menu_page(
+                    $this->page_title,
+                    $this->menu_title,
+                    $this->capability,
+                    $this->page_id,
+                    [$this, 'display_settings_page'],
+                    $this->menu_icon,
+                    $this->menu_position
+                );
+            } else {
+                $this->page = add_submenu_page(
+                    $this->menu_parent,
+                    $this->page_title,
+                    $this->menu_title,
+                    $this->capability,
+                    $this->page_id,
+                    [$this, 'display_settings_page'],
+                    $this->menu_position
+                );
+            }
+        }
+
+        public function display_settings_page() {
+            $this->register_settings_fields();
+
+            $tabs = $this->get_tabs();
+            echo "<style>.settings-wrap .tab-wrapper {display: none;}.settings-wrap .tab-wrapper.tab-active {display: block;}</style>";
+            echo '<div class="wrap settings-wrap ' . $this->page_id . '-wrap">';
+            echo '<h1>' . $this->page_title . '</h1>';
+            echo '<div id="settings-tabs-wrapper" class="nav-tab-wrapper">';
+
+            foreach ($tabs as $tab_id => $tab) {
+                if (empty($tab['sections']) && !isset($tab['render_callback'])) {
+                    continue;
+                }
+
+                $active_class = '';
+
+                if ('general' === $tab_id) {
+                    $active_class = ' nav-tab-active';
+                }
+
+                echo "<a id='tab-nav-{$tab_id}' class='nav-tab{$active_class}' href='#tab-{$tab_id}'>{$tab['label']}</a>";
+            }
+            echo '</div>';
+            echo '<form id="' . $this->page_id . '-settings-form" method="post" action="options.php">';
+
+            settings_fields($this->page_id);
+
+            foreach ($tabs as $tab_id => $tab) {
+                if (empty($tab['sections']) && !isset($tab['render_callback'])) {
+                    continue;
+                }
+
+                $active_class = '';
+
+                if ('general' === $tab_id) {
+                    $active_class = ' tab-active';
+                }
+
+                echo "<div id='tab-{$tab_id}' class='tab-wrapper{$active_class}'>";
+                if (!empty($tab['render_callback']) && \is_callable($tab['render_callback'])) {
+                    $tab['render_callback']();
+                }
+                $first_section = true;
+                if (!empty($tab['sections'])) {
+                    foreach ($tab['sections'] as $section_id => $section) {
+                        $full_section_id = $this->page_id . '_' . $section_id . '_section';
+                        if (false === $first_section) {
+                            echo '<hr>';
+                        }
+                        $first_section = false;
+                        if (!empty($section['label'])) {
+                            echo "<h2>{$section['label']}</h2>";
+                        }
+
+                        if (!empty($section['callback'])) {
+                            $section['callback']();
+                        }
+
+                        echo '<table class="form-table">';
+
+                        do_settings_fields($this->page_id, $full_section_id);
+
+                        echo '</table>';
+                    }
+                }
+                echo '</div>';
+            }
+
+            submit_button();
+            echo '</form>';
+            echo '</div>';
+        }
+
+        /**
+         * @param $name
+         * @param $key
+         * @param $field
+         * @param $values
+         * @return mixed
+         */
+        public function field_args($name, $key, $field, $values = false) {
+            $field['name']  = $name;
+            $field['std']   = isset($field['std']) ? $field['std'] : '';
+            $field['value'] = $values[$key] ?? $field['std'];
+            $field['id']    = sanitize_title($field['name']);
+
+            return $field;
+        }
+
+        /**
+         * @return mixed
+         */
+        public function get_page() {
+            return $this->page;
+        }
+
+        /**
+         * @param $field_id
+         * @param $field
+         * @param $section_id
+         * @param $prefix
+         * @param false $value
+         * @return null
+         */
+        public function register_setting_field($field_id, $field, $section_id, $prefix = false, $value = false) {
+            $controls_class_name = '\Sovit\Controls';
+            if (false === $prefix) {
+                $prefix = $this->setting_key;
+            }
+            $prefix        = $prefix . '[' . $field_id . ']';
+            $full_field_id = $prefix . '_' . $field_id;
+            if (isset($field['fields']) && !empty($field['fields'])) {
+                foreach ($field['fields'] as $subfield_id => $subfield) {
+                    $value = $value[$field_id] ?? [];
+                    $this->register_setting_field($subfield_id, $subfield, $section_id, $prefix, $value);
+                }
+
+                return;
+            }
+            $field['field_args']['id'] = $field_id;
+            $field['field_args']       = apply_filters('wppress/settings/field_args', $prefix, $field_id, $field['field_args'], $value);
+
+            $field_classes = [];
+
+            if (!empty($field['class'])) {
+                $field_classes[] = $field['field_args']['class'];
+            }
+
+            $field['field_args']['class'] = implode(' ', $field_classes);
+            $render_callback              = [$controls_class_name, 'render'];
+            if (!empty($field['render_callback'])) {
+                $render_callback = $field['render_callback'];
+            }
+
+            add_settings_field(
+                $field['field_args']['id'],
+                $field['label'] ?? '',
+                $render_callback,
+                $this->page_id,
+                $section_id,
+                $field['field_args']
+            );
+        }
+
+        public function register_settings_fields() {
+            $tabs = $this->get_tabs();
+
+            foreach ($tabs as $tab_id => $tab) {
+                if (!isset($tab['sections'])) {
+                    continue;
+                }
+
+                foreach ($tab['sections'] as $section_id => $section) {
+                    $full_section_id = $this->page_id . '_' . $section_id . '_section';
+
+                    $label = isset($section['label']) ? $section['label'] : '';
+
+                    $section_callback = isset($section['callback']) ? $section['callbak'] : '__return_empty_string';
+
+                    add_settings_section($full_section_id, $label, $section_callback, $this->page_id);
+
+                    foreach ($section['fields'] as $field_id => $field) {
+                        $this->register_setting_field($field_id, $field, $full_section_id, $this->setting_key, get_option($this->setting_key));
+                    }
+                }
+            }
+            register_setting($this->page_id, $this->setting_key, []);
+        }
+
+        /**
+         * @param $capability
+         * @return mixed
+         */
+        public function set_capability($capability) {
+            $this->capability = $capability;
+            return $this;
+        }
+
+        /**
+         * @param $icon
+         * @return mixed
+         */
+        public function set_icon($icon) {
+            $this->menu_icon = $icon;
+            return $this;
+        }
+
+        /**
+         * @param $parent
+         * @return mixed
+         */
+        public function set_menu_parent($parent) {
+            $this->menu_parent = $parent;
+            return $this;
+        }
+
+        /**
+         * @param $position
+         * @return mixed
+         */
+        public function set_menu_position($position) {
+            $this->menu_position = $position;
+            return $this;
+        }
+
+        /**
+         * @param $title
+         * @return mixed
+         */
+        public function set_menu_title($title) {
+            $this->menu_title = $title;
+            return $this;
+        }
+
+        /**
+         * @param $id
+         * @return mixed
+         */
+        public function set_page_id($page_id) {
+            $this->page_id = $page_id;
+            return $this;
+        }
+
+        /**
+         * @param $title
+         * @return mixed
+         */
+        public function set_page_title($title) {
+            $this->page_title = $title;
+            return $this;
+        }
+
+        /**
+         * @param $key
+         * @return mixed
+         */
+        public function set_setting_key($key) {
+            $this->setting_key = $key;
+            return $this;
+        }
+
+        protected function create_tabs() {
+            $tabs = [
+                'general' => [
+                    'label'    => esc_html__('General'),
+                    'sections' => [],
+                ],
+            ];
+            // filter to create tabs wppress/settings/{PAGE_ID}/tabs
+            return apply_filters('wppress/settings/' . $this->page_id . '/tabs', $tabs);
+        }
+
+        /**
+         * @return mixed
+         */
+        protected function get_tabs() {
+            $this->ensure_tabs();
+
+            return $this->tabs;
+        }
+
+        private function ensure_tabs() {
+            if (null === $this->tabs) {
+                $this->tabs = $this->create_tabs();
+                // action hook wppress/{PAGE_ID}/after_create_settings to add more settings
+                do_action('wppress/settings/' . $this->page_id . '/after_create_settings', $this);
+            }
+        }
+
+        /**
+         * @param $file
+         */
+        private function get_file_url($file = __FILE__) {
+            $file_path = str_replace("\\", "/", str_replace(str_replace("/", "\\", WP_CONTENT_DIR), "", $file));
+            if ($file_path) {
+                return content_url($file_path);
+            }
+
+            return false;
+        }
+    }
+}
